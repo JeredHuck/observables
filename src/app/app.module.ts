a6a17521f31da3f6ea812bb05f1e5ca4c9ea6617
JeredHuck <jeredhuck@gmail.com>
2023-10-26 00:08:38 -0500

    my observables work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
